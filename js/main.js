c97f9b7f3dbcf01a360cdc7f04fd296034d97481
Daniel Ramirez <daniel.rmz@gmail.com>
2013-07-01 23:47:57 -0700

    Countdown

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 
+$(function() {
+
+	var $countdown = $(".days-left");
+	var wedding_day = new Date(2014,6,5);
+	var today = new Date();
+
+	var daysLeft = (wedding_day - today)/1000/60/60/24;
+
+	$countdown.text(parseInt(daysLeft));
+
+});
